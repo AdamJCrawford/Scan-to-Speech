ac274470365019876ca191a4a604dcd964068a32
Torrance Zhou <113218481+torrancefzhou@users.noreply.github.com>
2023-04-08 15:24:18 -0700

    working camera preview

--- scan-to-speech/app/src/main/java/com/example/myscantospeech/MainActivity.kt
+++ scan-to-speech/app/src/main/java/com/example/myscantospeech/MainActivity.kt
@@ -1,19 +1,114 @@
 package com.example.myscantospeech
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.mlkit.vision.MlKitAnalyzer
+import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import com.google.mlkit.vision.common.InputImage
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.myscantospeech.databinding.ActivityMainBinding
 import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.bundled.latin.BundledLatinTextRecognizerCreator
+import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var textRecognizer: TextRecognizer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun startCamera() {
+        var cameraController = LifecycleCameraController(baseContext)
+        val previewView: PreviewView = viewBinding.viewFinder
+
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        cameraController.setImageAnalysisAnalyzer(
+            ContextCompat.getMainExecutor(this),
+            MlKitAnalyzer(
+                listOf(textRecognizer),
+                COORDINATE_SYSTEM_VIEW_REFERENCED,
+                ContextCompat.getMainExecutor(this)
+            ) { result: MlKitAnalyzer.Result? ->
+                val textRecognitionResults = result?.getValue(textRecognizer)
+                if ((textRecognitionResults == null) // ||
+                    // (textRecognitionResults.size == 0) ||
+                    // (textRecognitionResults.first() == null)
+                ) {
+                    previewView.overlay.clear()
+                    previewView.setOnTouchListener { _, _ -> false } //no-op
+                    return@MlKitAnalyzer
+                }
+
+                // val qrCodeViewModel = QrCodeViewModel(barcodeResults[0])
+                // val qrCodeDrawable = QrCodeDrawable(qrCodeViewModel)
+
+                // previewView.setOnTouchListener(qrCodeViewModel.qrCodeTouchCallback)
+                // previewView.overlay.clear()
+                // previewView.overlay.add(qrCodeDrawable)
+            }
+        )
+
+        cameraController.bindToLifecycle(this)
+        previewView.controller = cameraController
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+        textRecognizer.close()
+    }
+
+    companion object {
+        private const val TAG = "CameraX-MLKit"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA
+            ).toTypedArray()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
